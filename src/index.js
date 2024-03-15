6110281881d7c13a13d4aa0c961cd419dddb3b56
D-coder101 <bellofavour53@gmail.com>
2024-03-15 17:38:28 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Classy from './Classy-v1';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <Classy />
+  </React.StrictMode>
+);
+
+
